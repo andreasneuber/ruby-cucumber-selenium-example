953b7a7967633f455f448897abb41ebbb24140e1
Andreas Neuber <github@andy-neuber.de>
2024-08-12 11:38:07 +0200

    Update pipeline-assets/scripts/install-chrome-driver.sh

--- pipeline-assets/scripts/install-chrome-driver.sh
+++ pipeline-assets/scripts/install-chrome-driver.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-version=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")
-wget -N https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-chmod +x chromedriver
-cp chromedriver /usr/local/bin
-rm chromedriver_linux64.zip
-rm chromedriver
+version=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_STABLE)
+wget -N https://storage.googleapis.com/chrome-for-testing-public/${version}/linux64/chromedriver-linux64.zip
+
+unzip chromedriver-linux64.zip
+chmod +x chromedriver-linux64/chromedriver
+cp chromedriver-linux64/chromedriver /usr/local/bin
+
+rm chromedriver-linux64.zip
+rm -rf chromedriver-linux64
